69c01e235ba19ed184f639fc10b11baf1c893dcb
Job Noorman <job.noorman@cs.kuleuven.be>
2012-09-24 13:21:48 +0200

    Added simple SPM test.

---
+++ core/sim/rtl_sim/src-c/spm-test/main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <msp430.h>
+
+#define __spm __attribute__((section(".spm.public")))
+#define __secret __attribute__((section(".spm.secret")))
+
+extern char spm_public_start, spm_public_end, spm_secret_start, spm_secret_end;
+
+int __secret secret = 0;
+
+void __spm spm()
+{
+    puts("Writing secret from spm...");
+    secret = 0xbabe;
+    puts("...OK");
+}
+
+void write_secret()
+{
+    puts("Writing secret from unprotected...");
+    secret = 0xdead;
+    puts("...OK");
+}
+
+void protect_spm()
+{
+    puts("Protecting SPM");
+    asm("mov %0, r12\n"
+        "mov %1, r13\n"
+        "mov %2, r14\n"
+        "mov %3, r15\n"
+        ".word 0x1381\n"
+        :
+        : "i"(&spm_public_start), "i"(&spm_public_end),
+          "i"(&spm_secret_start), "i"(&spm_secret_end));
+    puts("...OK");
+}
+
+void unprotect_spm()
+{
+    puts("Unprotecting SPM");
+    asm(".word 0x1380");
+    puts("...OK");
+}
+
+void init_io()
+{
+    P1DIR = 0xff;
+    P2DIR = 0xff;
+}
+
+int main(void)
+{
+    init_io();
+    puts("main() started");
+
+    write_secret();
+    protect_spm();
+    spm();
+    write_secret();
+//     unprotect_spm();
+    write_secret();
+
+    puts("main() done");
+    P2OUT = 0xff;
+    return 0;
+}
+
+int putchar(int c)
+{
+    P1OUT = c;
+    P1OUT |= 0x80;
+    return c;
+}
---
+++ core/sim/rtl_sim/src-c/spm-test/spm-test.v
@@ -0,0 +1,28 @@
+`define NO_TIMEOUT
+
+initial
+begin
+    $display(" ===============================================");
+    $display("|                 START SIMULATION              |");
+    $display(" ===============================================");
+
+    //---------------------------------------
+    // Check CPU configuration
+    //---------------------------------------
+    if ((`PMEM_SIZE !== 61440) || (`DMEM_SIZE !== 2048))
+    begin
+        $display(" ===============================================");
+        $display("|               SIMULATION ERROR                |");
+        $display("|                                               |");
+        $display("|  Core must be configured for:                 |");
+        $display("|               - 60kB program memory           |");
+        $display("|               - 2kB data memory              |");
+        $display(" ===============================================");
+        $finish;
+    end
+end
+
+always @(posedge p1_dout[7])
+begin
+    $write("%c", p1_dout[6:0]);
+end
