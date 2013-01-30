8c4faaf989839b74b59afedee13659766e6da9a6
Job Noorman <job.noorman@cs.kuleuven.be>
2013-01-30 16:18:27 +0100

    Small code cleanup

--- core/rtl/verilog/omsp_spm.v
+++ core/rtl/verilog/omsp_spm.v
@@ -4,146 +4,117 @@
 `endif
 
 module omsp_spm(
-    mclk,
-    puc_rst,
-    pc,
-    prev_pc,
-    eu_mab,
-    eu_mb_en,
-    eu_mb_wr,
-    update_spm,
-    enable_spm,
-    check_new_spm,
-    next_id,
-    r12,
-    r13,
-    r14,
-    r15,
-    data_request,
-    spm_select,
-    write_key,
-    key_in,
-
-    enabled,
-    violation,
-    selected,
-    requested_data,
-    key_out
+  input  wire         mclk,
+  input  wire         puc_rst,
+  input  wire  [15:0] pc,
+  input  wire  [15:0] prev_pc,
+  input  wire  [15:0] eu_mab,
+  input  wire         eu_mb_en,
+  input  wire   [1:0] eu_mb_wr,
+  input  wire         update_spm,
+  input  wire         enable_spm,
+  input  wire         check_new_spm,
+  input  wire  [15:0] next_id,
+  input  wire  [15:0] r12,
+  input  wire  [15:0] r13,
+  input  wire  [15:0] r14,
+  input  wire  [15:0] r15,
+  input  wire   [2:0] data_request,
+  input  wire  [15:0] spm_select,
+  input  wire         write_key,
+  input  wire  [15:0] key_in,
+  output reg          enabled,
+  output wire         violation,
+  output wire         selected,
+  output reg   [15:0] requested_data,
+  output wire [0:127] key_out
 );
-
-input        mclk;
-input        puc_rst;
-input [15:0] pc;        // Program Counter
-input [15:0] prev_pc;
-input [15:0] eu_mab;    // Execution Unit Memory address bus
-input        eu_mb_en;  // Execution Unit Memory bus enable
-input  [1:0] eu_mb_wr;  // Execution Unit Memory bus write transfer
-input        update_spm;
-input        enable_spm;
-input        check_new_spm;
-input [15:0] next_id;
-input [15:0] r12;
-input [15:0] r13;
-input [15:0] r14;
-input [15:0] r15;
-input  [2:0] data_request;
-input [15:0] spm_select;
-input        write_key;
-input [15:0] key_in;
-
-output            enabled;
-output            violation;
-output            selected;
-output reg [15:0] requested_data;
-
-output wire [0:127] key_out;
 
 reg [15:0] id;
 reg [15:0] public_start;
 reg [15:0] public_end;
 reg [15:0] secret_start;
 reg [15:0] secret_end;
-reg        enabled;
 
 reg   [3:0] key_idx;
 reg [0:127] key;
 
 function exec_spm;
-    input [15:0] current_pc;
+  input [15:0] current_pc;
 
-    begin
-        exec_spm = current_pc >= public_start & current_pc < public_end;
-    end
+  begin
+    exec_spm = current_pc >= public_start & current_pc < public_end;
+  end
 endfunction
 
 function do_overlap;
-    input [15:0] start_a;
-    input [15:0] end_a;
-    input [15:0] start_b;
-    input [15:0] end_b;
+  input [15:0] start_a;
+  input [15:0] end_a;
+  input [15:0] start_b;
+  input [15:0] end_b;
 
-    begin
-        do_overlap = (start_a < end_b) & (end_a > start_b);
-    end
+  begin
+    do_overlap = (start_a < end_b) & (end_a > start_b);
+  end
 endfunction
 
 initial
 begin
-    public_start = 0;
-    public_end = 0;
-    secret_start = 0;
-    secret_end = 0;
-    enabled = 0;
+  public_start = 0;
+  public_end = 0;
+  secret_start = 0;
+  secret_end = 0;
+  enabled = 0;
 end
 
 always @(posedge mclk or posedge puc_rst)
 begin
-    if (puc_rst)
+  if (puc_rst)
+  begin
+    id <= 0;
+    public_start <= 0;
+    public_end <= 0;
+    secret_start <= 0;
+    secret_end <= 0;
+    enabled <= 0;
+    key_idx <= 0;
+  end
+  else if (update_spm)
+  begin
+    if (enable_spm)
     begin
-        id <= 0;
-        public_start <= 0;
-        public_end <= 0;
-        secret_start <= 0;
-        secret_end <= 0;
-        enabled <= 0;
+      if ((r12 < r13) & (r14 < r15))
+      begin
+        id <= next_id;
+        public_start <= r12;
+        public_end <= r13;
+        secret_start <= r14;
+        secret_end <= r15;
+        enabled <= 1;
         key_idx <= 0;
+        $display("New SPM config: %h %h %h %h", r12, r13, r14, r15);
+      end
+      else
+      begin
+        $display("Invalid SPM config: %h %h %h %h", r12, r13, r14, r15);
+      end
     end
-    else if (update_spm)
+    else if (pc >= public_start && pc < public_end)
     begin
-        if (enable_spm)
-        begin
-            if ((r12 < r13) & (r14 < r15))
-            begin
-                id <= next_id;
-                public_start <= r12;
-                public_end <= r13;
-                secret_start <= r14;
-                secret_end <= r15;
-                enabled <= 1;
-                key_idx <= 0;
-                $display("New SPM config: %h %h %h %h", r12, r13, r14, r15);
-            end
-            else
-            begin
-                $display("Invalid SPM config: %h %h %h %h", r12, r13, r14, r15);
-            end
-        end
-        else if (pc >= public_start && pc < public_end)
-        begin
-            id <= 0;
-            public_start <= 0;
-            public_end <= 0;
-            secret_start <= 0;
-            secret_end <= 0;
-            enabled <= 0;
-            $display("SPM disabled");
-        end
+      id <= 0;
+      public_start <= 0;
+      public_end <= 0;
+      secret_start <= 0;
+      secret_end <= 0;
+      enabled <= 0;
+      $display("SPM disabled");
     end
-    else if (selected & write_key)
-    begin
-        key[16*key_idx+:16] <= key_in;
-        key_idx <= key_idx + 1;
-    end
+  end
+  else if (selected & write_key)
+  begin
+    key[16*key_idx+:16] <= key_in;
+    key_idx <= key_idx + 1;
+  end
 end
 
 wire exec_public = exec_spm(pc);
@@ -155,23 +126,23 @@ wire create_violation = check_new_spm &
                          do_overlap(r12, r13, secret_start, secret_end) |
                          do_overlap(r14, r15, public_start, public_end) |
                          do_overlap(r14, r15, secret_start, secret_end));
-wire violation = enabled & (mem_violation | exec_violation | create_violation);
+assign violation = enabled & (mem_violation | exec_violation | create_violation);
 
 always @(posedge mclk)
 begin
-    if (violation)
+  if (violation)
+  begin
+    if (mem_violation)
+      $display("mem violation @%h, from %h", eu_mab, pc);
+    else if (exec_violation)
+      $display("exec violation %h -> %h", prev_pc, pc);
+    else if (create_violation)
     begin
-        if (mem_violation)
-            $display("mem violation @%h, from %h", eu_mab, pc);
-        else if (exec_violation)
-            $display("exec violation %h -> %h", prev_pc, pc);
-        else if (create_violation)
-        begin
-            $display("create violation:");
-            $display("\tme:  %h %h %h %h", public_start, public_end, secret_start, secret_end);
-            $display("\tnew: %h %h %h %h", r12, r13, r14, r15);
-        end
+      $display("create violation:");
+      $display("\tme:  %h %h %h %h", public_start, public_end, secret_start, secret_end);
+      $display("\tnew: %h %h %h %h", r12, r13, r14, r15);
     end
+  end
 end
 
 // FIXME: WTF? This somehow doesn't work when executing HKDF
@@ -182,15 +153,15 @@ assign key_out = selected ? key : 128'bz;
 
 always @(*)
   if (selected)
-    case (data_request)
-      `SPM_REQ_PUBSTART: requested_data = public_start;
-      `SPM_REQ_PUBEND:   requested_data = public_end;
-      `SPM_REQ_SECSTART: requested_data = secret_start;
-      `SPM_REQ_SECEND:   requested_data = secret_end;
-      `SPM_REQ_ID:       requested_data = id;
-      default:           requested_data = 16'bx;
-    endcase
+  case (data_request)
+    `SPM_REQ_PUBSTART: requested_data = public_start;
+    `SPM_REQ_PUBEND:   requested_data = public_end;
+    `SPM_REQ_SECSTART: requested_data = secret_start;
+    `SPM_REQ_SECEND:   requested_data = secret_end;
+    `SPM_REQ_ID:       requested_data = id;
+    default:           requested_data = 16'bx;
+  endcase
   else
-    requested_data = 16'bz;
+  requested_data = 16'bz;
 
 endmodule
--- core/rtl/verilog/omsp_spm_control.v
+++ core/rtl/verilog/omsp_spm_control.v
@@ -4,50 +4,27 @@
 `endif
 
 module omsp_spm_control(
-    mclk,
-    puc_rst,
-    pc,
-    eu_mab,
-    eu_mb_en,
-    eu_mb_wr,
-    update_spm,
-    enable_spm,
-    r12,
-    r13,
-    r14,
-    r15,
-    data_request,
-    spm_select,
-    write_key,
-    key_in,
-    violation,
-    spm_select_valid,
-    requested_data,
-    key_out
+  input  wire         mclk,
+  input  wire         puc_rst,
+  input  wire  [15:0] pc,
+  input  wire  [15:0] eu_mab,
+  input  wire         eu_mb_en,
+  input  wire   [1:0] eu_mb_wr,
+  input  wire         update_spm,
+  input  wire         enable_spm,
+  input  wire  [15:0] r12,
+  input  wire  [15:0] r13,
+  input  wire  [15:0] r14,
+  input  wire  [15:0] r15,
+  input  wire  [15:0] spm_select,
+  input  wire   [2:0] data_request,
+  input  wire         write_key,
+  input  wire  [15:0] key_in,
+  output wire         violation,
+  output wire         spm_select_valid,
+  output wire  [15:0] requested_data,
+  output wire [0:127] key_out
 );
-
-input        mclk;
-input        puc_rst;
-input [15:0] pc;        // Program Counter
-input [15:0] eu_mab;    // Execution Unit Memory address bus
-input        eu_mb_en;  // Execution Unit Memory bus enable
-input  [1:0] eu_mb_wr;  // Execution Unit Memory bus write transfer
-input        update_spm;
-input        enable_spm;
-input [15:0] r12;
-input [15:0] r13;
-input [15:0] r14;
-input [15:0] r15;
-input [15:0] spm_select;
-input  [2:0] data_request;
-input        write_key;
-input [15:0] key_in;
-
-output             violation;
-output             spm_select_valid;
-output wire [15:0] requested_data;
-
-output wire [0:127] key_out;
 
 // input to the SPM array. indicates which SPM(s) should be updated. when a new
 // SPM is being created, only one bit will be 1. if an SPM is being destroyed,
@@ -61,11 +38,8 @@ wire [0:`NB_SPMS-1] spms_first_disabled;
 wire [0:`NB_SPMS-1] spms_enabled;
 // output of the SPM array. violations detected by the SPMs
 wire [0:`NB_SPMS-1] spms_violation;
-// helper wire to detect a violation
-wire violation;
 
 wire [0:`NB_SPMS-1] spms_selected;
-wire [16*`NB_SPMS-1:0] spms_requested_data;
 
 reg [15:0] current_pc, prev_pc;
 reg [15:0] next_id;
@@ -78,54 +52,53 @@ assign spms_check = (update_spm & enable_spm) ? (~spms_update & spms_enabled)
                                               : `NB_SPMS'b0;
 
 always @(posedge mclk or posedge puc_rst)
-    if (puc_rst)
-        next_id <= 16'h1;
-    else if (update_spm && enable_spm)
-        next_id <= next_id + 16'h1;
+  if (puc_rst)
+    next_id <= 16'h1;
+  else if (update_spm && enable_spm)
+    next_id <= next_id + 16'h1;
 
 assign violation = |spms_violation || (next_id == 16'h0);
 
 generate
-    genvar i;
-    assign spms_first_disabled[0] = ~spms_enabled[0];
-    for (i = 1; i < `NB_SPMS; i = i + 1)
-        assign spms_first_disabled[i] = ~spms_enabled[i] & ~|spms_first_disabled[0:i-1];
+  genvar i;
+  assign spms_first_disabled[0] = ~spms_enabled[0];
+  for (i = 1; i < `NB_SPMS; i = i + 1)
+    assign spms_first_disabled[i] = ~spms_enabled[i] & ~|spms_first_disabled[0:i-1];
 endgenerate
 
 assign spm_select_valid = |spms_selected;
 
 always @(pc)
 begin
-    prev_pc <= current_pc;
-    current_pc <= pc;
+  prev_pc <= current_pc;
+  current_pc <= pc;
 end
 
 omsp_spm omsp_spms[0:`NB_SPMS-1](
-    .mclk               (mclk),
-    .puc_rst            (puc_rst),
-    .pc                 (pc),
-    .prev_pc            (prev_pc),
-    .eu_mab             (eu_mab),
-    .eu_mb_en           (eu_mb_en),
-    .eu_mb_wr           (eu_mb_wr),
-    .update_spm         (spms_update),
-    .enable_spm         (enable_spm),
-    .check_new_spm      (spms_check),
-    .next_id            (next_id),
-    .r12                (r12),
-    .r13                (r13),
-    .r14                (r14),
-    .r15                (r15),
-    .data_request       (data_request),
-    .spm_select         (spm_select),
-    .write_key          (write_key),
-    .key_in             (key_in),
-
-    .enabled            (spms_enabled),
-    .violation          (spms_violation),
-    .selected           (spms_selected),
-    .requested_data     (requested_data),
-    .key_out            (key_out)
+  .mclk           (mclk),
+  .puc_rst        (puc_rst),
+  .pc             (pc),
+  .prev_pc        (prev_pc),
+  .eu_mab         (eu_mab),
+  .eu_mb_en       (eu_mb_en),
+  .eu_mb_wr       (eu_mb_wr),
+  .update_spm     (spms_update),
+  .enable_spm     (enable_spm),
+  .check_new_spm  (spms_check),
+  .next_id        (next_id),
+  .r12            (r12),
+  .r13            (r13),
+  .r14            (r14),
+  .r15            (r15),
+  .data_request   (data_request),
+  .spm_select     (spm_select),
+  .write_key      (write_key),
+  .key_in         (key_in),
+  .enabled        (spms_enabled),
+  .violation      (spms_violation),
+  .selected       (spms_selected),
+  .requested_data (requested_data),
+  .key_out        (key_out)
 );
 
 endmodule
