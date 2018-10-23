4ca386e3c895e79a3fb7ee2806db2c71bb845518
S3rg7o <seminara.sergio7@gmail.com>
2018-10-23 16:18:35 +0200

    fixing Jo's mess

--- core/sim/rtl_sim/bin/asm2ihex.sh
+++ core/sim/rtl_sim/bin/asm2ihex.sh
@@ -107,7 +107,7 @@ fi
 #                  Compile, link & generate IHEX file                         #
 ###############################################################################
 echo ""
-echo "\$ $MSPGCC_PFX-as      -alsm $2 -o $1.o > $1.l43"
+echo "\$ $MSPGCC_PFX-as    -I ../src/sancus  -alsm $2 -o $1.o > $1.l43"
 $MSPGCC_PFX-as      -alsm         $2     -o $1.o     > $1.l43
 echo "\$ $MSPGCC_PFX-objdump -xdsStr $1.o >> $1.l43"
 $MSPGCC_PFX-objdump -xdsStr       $1.o              >> $1.l43
