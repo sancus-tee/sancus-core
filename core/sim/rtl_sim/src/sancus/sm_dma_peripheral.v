7a2bb21d93b425ccba1d93d8edb9ff29911955ce
marton bognar <martonbognar@sch.bme.hu>
2020-03-27 00:40:57 +0100

    Throw errors if registers do not contain the correct values

--- core/sim/rtl_sim/src/sancus/sm_dma_peripheral.v
+++ core/sim/rtl_sim/src/sancus/sm_dma_peripheral.v
@@ -14,9 +14,12 @@ initial
       while(~sm_0_executing) @(posedge mclk);
 
       /* ----------------------  END OF TEST --------------- */
-      @(r8==16'b0000000001000000);
-      @(r9==16'b0000000101000000);
       @(r15==16'h2000);
+
+      if(r8 !== 16'b0000000001000000)
+         tb_error("====== r8 does not mach expected value ======");
+      if(r9 !== 16'b0000000101000000)
+         tb_error("====== r9 does not mach expected value ======");
 
       stimulus_done = 1;
    end
