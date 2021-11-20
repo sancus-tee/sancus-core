e06db7dd8cec7c037640ccaf1ea03df4fa0f4316
marton bognar <marton.bognar@cs.kuleuven.be>
2021-11-20 17:44:47 +0100

    Add attacks B-2 to B-6

---
+++ core/sim/rtl_sim/src/gap-attacks/B-2-maxlen.v
@@ -0,0 +1,27 @@
+initial
+   begin
+      $display(" =============================================== ");
+      $display("|                 START SIMULATION              |");
+      $display(" =============================================== ");
+      $display("__SECRET=%d", `__SECRET);
+
+      repeat(5) @(posedge mclk);
+      stimulus_done = 0;
+
+      /* ----------------------  SM INITIALIZATION --------------- */
+      $display("\n--- SM INIT ---");
+      @(posedge crypto_start);
+      @(posedge exec_done);
+      repeat(2) @(posedge mclk);
+      $display("SM enabled");
+      if (!sm_0_enabled)                    tb_error("====== SM NOT ENABLED ======");
+
+      $display("\n--- SM ENTRY ---");
+      @(posedge sm_0_executing);
+
+      /* ----------------------  END OF TEST --------------- */
+      @(r15==16'h2000);
+
+      if (r14!=`__SECRET)                    tb_error("====== R14 FINAL ======");
+      stimulus_done = 1;
+   end
